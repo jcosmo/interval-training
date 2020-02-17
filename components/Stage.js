2e05e118334c7b7b7a9203506fed2f5dfc2fe957
James Walker <github@jzwalker.id.au>
2020-02-17 23:57:27 +1100

    Render stages differently, add a delete icon

--- components/Stage.js
+++ components/Stage.js
@@ -1,16 +1,35 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 
 const Stage = props => {
   return (
-    <View key={props.data.id} style={styles.stage}><Text>{props.data.text}</Text></View>
+    <View key={props.data.id} style={styles.stage}>
+      <Text style={styles.text}>{props.data.text}</Text>
+      <Image style={styles.deleteIcon} source={require("../assets/delete.png")}/>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   stage: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     marginVertical: 10,
     padding: 10,
+    borderColor: '#777',
+    borderWidth: 1,
+    backgroundColor: '#EEE',
+  },
+
+  text: {
+    flexGrow: 0,
+  },
+
+  deleteIcon: {
+    height: 15,
+    width: 15,
+    flexGrow: 0
   }
 });
 
